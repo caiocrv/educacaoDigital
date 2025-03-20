612cd0b3f9a109cac119c9b1eaab9a68ee16a606
Caio Gomes Carvalho <caio.gcarvalho@gruposaga.com.br>
2025-03-20 08:15:40 -0300

    Armazenar login e senha

--- plataformaEducacaoDigital/main.py
+++ plataformaEducacaoDigital/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+
 import json
 
 app = Flask(__name__)
@@ -7,9 +8,27 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/teste")
-def teste():
-    return "página dois"
+@app.route("/login", methods=['POST'])
+def login():
+    # Armazenando o usuário e senha na variavel
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # Armazenando em um dicionário
+    user_data = {
+        "username":username,
+        "password":password
+    }
+
+    print(user_data)
+
+    return jsonify(user_data)
+
+
+
+
+
+
 
 if __name__ == '__main__':
-    app.run(host='192.168.100.56', port=500)
+    app.run(host='172.19.214.107', port=500)
