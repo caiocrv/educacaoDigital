7f974c74783be315bde8b1ef1d3b92ca4092b76f
Caio Gomes Carvalho <caio.gcarvalho@gruposaga.com.br>
2025-03-22 16:38:19 -0300

    Armazenando Usuarios

--- plataformaEducacaoDigital/main.py
+++ plataformaEducacaoDigital/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route("/login", methods=['POST'])
 def login():
@@ -14,31 +14,36 @@ def login():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    # Armazenando em um dicionário
-    user_data = {
-        "username":username,
-        "password":password
-    }
-
-    print(user_data)
-
-    return jsonify(user_data)
-
-@app.route("/cadastro", methods=['GET', 'POST'])
-def cadastro():
+@app.route("/register", methods=['GET', 'POST'])
+def register():
     if request.method == 'POST':
         password = request.form.get('password')
         confirmPassword = request.form.get('confirmPassword')
+        username = request.form.get('username')
 
-        if confirmPassword != password:
-            return render_template('cadastro.html', error="As senhas não coincidem.")
+        if confirmPassword == password:
+            # Armazenando em um dicionário
+            user_data = {
+                "username":username,
+                "password":password
+            }
 
-        return jsonify({"massege": "Cadastro realizado com sucesso!"})
-    
-    return render_template('cadastro.html')
+            with open("plataformaEducacaoDigital/data/users.json", "w", encoding="utf-8") as arquivo:
+                json.dump(user_data, arquivo, indent=4, ensure_ascii=False)
+            
+            print(user_data)
 
+            return render_template('login.html')
+        
+        elif confirmPassword != password:
+            return render_template('register.html', error="As senhas não coincidem.")
+                   
 
+    return render_template('register.html')
 
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
 
 
 
