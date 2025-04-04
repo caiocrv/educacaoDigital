54b8f3bdd864628085ac05dfba5945bacf03d976
unknown <caiogomescarvalhos@gmail.com>
2025-04-03 22:30:21 -0300

    Adicionando Token

--- plataformaEducacaoDigital/main.py
+++ plataformaEducacaoDigital/main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
+from argon2 import PasswordHasher
+import jwt
+import datetime
 
 import uuid
 import json
 import unicodedata
 
+ph = PasswordHasher()
 app = Flask(__name__)
 
 @app.route("/")
@@ -24,12 +28,28 @@ def login():
         return render_template('login.html', error="Usuário não encontrado.")
 
     # Validando credencial
-    if any(user['username'] == username for user in userRegistration) and any(keys['password'] == password for keys in userRegistration):
-        print("Bem vindo")
-        return render_template('home.html')
-    else:
-        print("Usuário ou senha incorreto")
+    try:
+        user = next((u for u in userRegistration if u['username'] == username), None)
+        
+        if not user:
+            return render_template('login.html', error="Usuário ou senha incorreto")
+        
+        if not ph.verify(user['password'], password):
+            return render_template('login.html', error="Usuário ou senha incorreto")
+    except:
         return render_template('login.html', error="Usuário ou senha incorreto")
+
+    # Gerando Token de acesso, usado para verificar a sessão do usuário
+    payload = {
+        "user_id":user['id'],
+        "user_name":user['username'],
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=24)  # Token expira em 24 hora
+    }
+
+    token = jwt.encode(payload, "123", algorithm="HS256")
+    print(token)
+
+    return render_template('home.html')
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -45,58 +65,58 @@ def register():
         characterSpecial = 0
         characterUpper = 0
 
-        if confirmPassword == password:
-            for character in password:
-                # Verificando se a senha tem backspace
-                if character.isspace():
-                    print("Senha não pode conter espaço")
-                    return render_template('register.html', error="Senha não pode conter espaço.")
-                
-                # Verificando a quantidade de caracteres especiais na senha
-                if character in "@#?!&":
-                    characterSpecial += 1
-                
-                # Verificando a quantidade de caracteres maíusculo
-                if character.isupper():
-                    characterUpper += 1
-
-            # Verificando dificuldade da senha
-            if len(password) >= 6 and characterSpecial >= 1 and characterUpper >= 1:
-                try:
-                    with open("plataformaEducacaoDigital/data/users.json", "r", encoding="utf-8") as arquivo:
-                        users_data = json.load(arquivo)
-                except FileNotFoundError:
-                    users_data = []
-
-                if any(user['username'] == username for user in users_data):
-                    print("Usuário já existe")
-                    return render_template('register.html', error="Usuário já existe.")
-                
-
-                user_id = str(uuid.uuid4())  # Gerando um UUID único
-
-                # Armazenando em um dicionário
-                user_data = {
-                    "id":user_id,
-                    "username":username,
-                    "password":password
-                }
-
-                users_data.append(user_data) # Adiciona o novo usuário
-
-                # Inserindo usuário e senha no arquivo JSON
-                with open("plataformaEducacaoDigital/data/users.json", "w", encoding="utf-8") as arquivo:
-                    json.dump(users_data, arquivo, indent=4, ensure_ascii=False)
-                
-                print(user_data)
-
-                return render_template('login.html')
-            else:
-                print("Senha não contém nível de dificuldade suficiente")
-                return render_template('register.html', error="Senha não possiu dificuldade mínima permitida")
-
-        else:
+        if confirmPassword != password:
             return render_template('register.html', error="As senhas não coincidem.")
+        
+        for character in password:
+            # Verificando se a senha tem backspace
+            if character.isspace():
+                print("Senha não pode conter espaço")
+                return render_template('register.html', error="Senha não pode conter espaço.")
+            
+            # Verificando a quantidade de caracteres especiais na senha
+            if character in "@#?!&":
+                characterSpecial += 1
+            
+            # Verificando a quantidade de caracteres maíusculo
+            if character.isupper():
+                characterUpper += 1
+
+        # Verificando dificuldade da senha
+        if len(password) >= 6 and characterSpecial >= 1 and characterUpper >= 1:
+            try:
+                with open("plataformaEducacaoDigital/data/users.json", "r", encoding="utf-8") as arquivo:
+                    users_data = json.load(arquivo)
+            except FileNotFoundError:
+                users_data = []
+
+            if any(user['username'] == username for user in users_data):
+                print("Usuário já existe")
+                return render_template('register.html', error="Usuário já existe.")
+            
+
+            user_id = str(uuid.uuid4())  # Gerando um UUID único
+            hashadPassword = ph.hash(password)
+
+            # Armazenando em um dicionário
+            user_data = {
+                "id":user_id,
+                "username":username,
+                "password":hashadPassword
+            }
+
+            users_data.append(user_data) # Adiciona o novo usuário
+
+            # Inserindo usuário e senha no arquivo JSON
+            with open("plataformaEducacaoDigital/data/users.json", "w", encoding="utf-8") as arquivo:
+                json.dump(users_data, arquivo, indent=4, ensure_ascii=False)
+            
+            print(user_data)
+
+            return render_template('login.html')
+        else:
+            print("Senha não contém nível de dificuldade suficiente")
+            return render_template('register.html', error="Senha não possiu dificuldade mínima permitida")
                    
 
     return render_template('register.html')
