3cbe03b92b19f6d991827098b247997ffa59c36d
unknown <caiogomescarvalhos@gmail.com>
2025-05-21 22:03:30 -0300

    padronizando indentacao

--- plataformaEducacaoDigital/main.py
+++ plataformaEducacaoDigital/main.py
@@ -21,6 +21,10 @@ def landingPage():
     if request.method == 'GET':
         return render_template('landingPage.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
